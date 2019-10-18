db1d0ab45fdfcaaa9f3acb03e8c82ba1f3a03d32
arieltraut <arieltraut@gmail.com>
2019-10-17 21:12:28 -0300

    login diseño

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FirebaseAuthService } from 'src/app/services/firebase-auth.service';
 
 @Component({
@@ -8,9 +8,27 @@ import { FirebaseAuthService } from 'src/app/services/firebase-auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  @ViewChild('alertError', { static: true }) alertError: ElementRef;
+  isLoading = false;
+
   constructor( public authService: FirebaseAuthService) { }
 
   ngOnInit() {
+  }
+
+  onLogin(userEmail, userPassword) {
+    this.isLoading = true;
+    this.authService.SignIn(userEmail, userPassword).then((result) => {
+      this.isLoading = false;
+    }).catch((error) => {
+      this.isLoading = false;
+      this.mostrarAlert();
+      console.info(error);
+    });
+  }
+
+  mostrarAlert() {
+    this.alertError.nativeElement.classList.remove('d-none');
   }
 
 }
